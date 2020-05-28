f1583e7f28b707c1a9ed14bd3758ba15de997cba
carolinevianab <carolinevb123@gmail.com>
2020-05-28 13:09:03 -0300

    No processo de criação de tela de onboarding, tab bar já criada

---
+++ Musitab/Musitab/OnBoardingViewController.swift
@@ -0,0 +1,49 @@
+//
+//  OnBoardingViewController.swift
+//  Musitab
+//
+//  Created by Caroline Viana on 28/05/20.
+//  Copyright © 2020 Caroline Viana. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+
+    @IBOutlet weak var message: UILabel!
+    var cont = 0
+    let Msg = ["Já se perguntou:\n\"O que eu vou tocar hoje?\"\nSe sim...\nVamos te ajudar com esse probleminha!", "Clique no seu estilo favorito, ou arrisque no aleatório!\nBora lá?"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+    }
+    @IBAction func next(_ sender: Any) {
+        cont += 1
+        if(cont < 1){
+            message.text = Msg[cont]
+        }
+        else{
+            cont = 0
+        }
+        
+    }
+    
+    func endOnBoarding(){
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
